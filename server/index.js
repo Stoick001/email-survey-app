16d0f2a4e519e0de8114fa72189393efa56bc5cd
Stoick01 <work.gordan.prastalo@gmail.com>
2018-01-27 16:06:07 +0100

    Added oauth verification with passport.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'hello'});
-});
+require('./routes/auth-routes')(app);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`);
+});

a88e92dd63e423c8542c15020d88379acad4781c
Stoick01 <work.gordan.prastalo@gmail.com>
2018-02-17 11:48:52 +0100

    Added billing client side app

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
+
 const keys = require('./config/keys')
 
 require('./models/user');
@@ -11,6 +13,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(cookieSession({
   mexAge: 30 * 24 * 60 * 60 * 1000,
   keys: [keys.cookieKey]
@@ -20,6 +23,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/auth-routes')(app);
+require('./routes/billing-routes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'server', 'client', 'bild', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- server/services/passport.js
+++ server/services/passport.js
@@ -20,16 +20,11 @@ passport.use(new GoogleStrategy({
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
-      if (existingUser) {
-        done(null, existingUser);
-      } else {
-          new User({ googleId: profile.id }).save()
-            .then((user) => done(null, user))
-            .catch((e) => console.log(e));
-      }
-    })
-    .catch((e) => console.log(e));
+}, async (accessToken, refreshToken, profile, done) => {
+  const existingUser = await User.findOne({ googleId: profile.id });
+  if (existingUser) {
+    return done(null, existingUser);
+  }
+  const user = await new User({ googleId: profile.id }).save();
+  done(null, user);
 }));
